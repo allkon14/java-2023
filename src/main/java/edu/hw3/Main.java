34d734952433de6855a86d6cdfa8c144d6254b1d
allkon14 <alena.konoplevaa@yandex.ru>
2023-10-28 23:41:24 +0400

    hw3 - 4

--- src/main/java/edu/hw3/Main.java
+++ src/main/java/edu/hw3/Main.java
@@ -1,10 +1,10 @@
 package edu.hw3;
 
-import static edu.hw3.Task1.atbash;
-import static edu.hw3.Task2.clusterize;
-import static edu.hw3.Task5.ContactBook.parseContacts;
-import static edu.hw3.Task4.convertToRoman;
-import static edu.hw3.Task3.freqDict;
+//import static edu.hw3.Task1.atbash;
+//import static edu.hw3.Task2.clusterize;
+//import static edu.hw3.Task5.ContactBook.parseContacts;
+//import static edu.hw3.Task4.convertToRoman;
+//import static edu.hw3.Task3.freqDict;
 
 public class Main {
 
@@ -13,19 +13,19 @@ public class Main {
 
 
     public static void main(String[] args) {
-        atbash("Hello world!");
-        System.out.println(clusterize(("()()()")));
-        System.out.println(clusterize("((()))"));
-        System.out.println(clusterize(("((()))(())()()(()())")));
-        System.out.println(clusterize(("((())())(()(()()))")));
-
-
-        System.out.println(freqDict(new String[] {"a", "bb", "a", "bb"}));
-
-        System.out.println(convertToRoman(2));
-        System.out.println(convertToRoman(2089));
-
-        System.out.println(parseContacts(new String[] {"Paul Erdos", "Leonhard Euler", "Carl Gauss"}, "DESC"));
+//        atbash("Hello world!");
+//        System.out.println(clusterize(("()()()")));
+//        System.out.println(clusterize("((()))"));
+//        System.out.println(clusterize(("((()))(())()()(()())")));
+//        System.out.println(clusterize(("((())())(()(()()))")));
+//
+//
+//        System.out.println(freqDict(new String[] {"a", "bb", "a", "bb"}));
+//
+//        System.out.println(convertToRoman(2));
+//        System.out.println(convertToRoman(2089));
+//
+//        System.out.println(parseContacts(new String[] {"Paul Erdos", "Leonhard Euler", "Carl Gauss"}, "DESC"));
 
     }
 }
