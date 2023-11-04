c84dc7cf27191b09951f42d7b19f61f10f5b4e0f
allkon14 <alena.konoplevaa@yandex.ru>
2023-11-04 22:35:08 +0400

    правки по hw1 (2)

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Task1 {
     private static final Logger LOGGER = LogManager.getLogger();
+
     public static int minutesToSeconds(String timeStr) {
         var time = timeStr.split(":");
         if (time.length != 2) {
