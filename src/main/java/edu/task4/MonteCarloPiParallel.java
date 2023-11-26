356768ee80de0cbf1c5f9d6a176de9d521742d2b
allkon14 <alena.konoplevaa@yandex.ru>
2023-11-26 21:06:51 +0400

    hw7 (2)

---
+++ src/main/java/edu/task4/MonteCarloPiParallel.java
@@ -0,0 +1,45 @@
+package edu.task4;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MonteCarloPiParallel {
+    private static final AtomicLong insideCircle = new AtomicLong(0);
+
+    public static double calculatePi(int numThreads, long numSimulations) {
+        insideCircle.set(0);
+
+        Thread[] threads = new Thread[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(() -> simulatePoints(numSimulations / numThreads));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        double ratio = insideCircle.get() / (double) numSimulations;
+
+        return 4 * ratio;
+    }
+
+    private static void simulatePoints(long numPoints) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        for (int i = 0; i < numPoints; i++) {
+            double x = random.nextDouble();
+            double y = random.nextDouble();
+            double distance = Math.sqrt(x * x + y * y);
+
+            if (distance <= 1) {
+                insideCircle.incrementAndGet();
+            }
+        }
+    }
+}
