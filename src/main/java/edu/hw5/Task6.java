cd2d3b23cc83b8f817ae210f3a0d08b51557ec9d
allkon14 <alena.konoplevaa@yandex.ru>
2023-11-30 00:22:12 +0400

    hw5 (правки(2))

--- src/main/java/edu/hw5/Task6.java
+++ src/main/java/edu/hw5/Task6.java
@@ -1,8 +1,5 @@
 package edu.hw5;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Task6 {
     public static boolean isSubstring(String sub, String src) {
         return src.matches(".*" + sub + ".*");
